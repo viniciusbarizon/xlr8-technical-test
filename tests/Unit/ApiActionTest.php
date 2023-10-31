f98078e72226922e41db4cbe4cb49c24414e5e08
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-31 16:25:06 +0000

    ApiActionTest

--- tests/Unit/ApiActionTest.php
+++ tests/Unit/ApiActionTest.php
@@ -3,10 +3,14 @@
 namespace Tests\Unit;
 
 use App\Actions\ApiAction;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use PHPUnit\Framework\TestCase;
 
 class ApiActionTest extends TestCase
 {
+    const BASE_URL = "https://xlr8-interview-files.s3.eu-west-2.amazonaws.com/";
+
     private array $apiHotels;
     private array $hotels;
 
@@ -25,17 +29,15 @@ class ApiActionTest extends TestCase
 
     private function setApiHotels(): void
     {
-        $client = new GuzzleHttp\Client();
-        $statusCode = 200;
+        $client = new Client();
+        $sourceId = 1;
 
-        while ($statusCode == 200) {
-            $sourceId = 1;
-            $res = $client->request(
-                "GET",
-                "https://xlr8-interview-files.s3.eu-west-2.amazonaws.com/source_$sourceId.json"
-            );
+        try {
+            $res = $client->request('GET', self::BASE_URL . 'source_' . $sourceId . '.json');
             $this->apiHotels[$sourceId] = json_decode($res->getBody(), true);
-            $statusCode = $res->getStatusCode();
+            $sourceId++;
+        } catch (ClientException $e) {
+            return;
         }
     }
 
