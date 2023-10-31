4a90ce65a2467388bcadb457d4dd08e0be311652
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-31 17:46:11 +0000

    SearchController index

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class SearchController extends Controller
 {
-    public function index() {
+    public function index(): View
+    {
         return view("index");
     }
 }
