b29fcc8c1c195fbeba9d2c1bf8ef4a21a9dde767
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-31 17:49:00 +0000

    SearchController: set hotels

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\ApiAction;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class SearchController extends Controller
 {
+    private array $hotels;
+
     public function index(): View
     {
         return view("index");
+    }
+
+    public function search(): View
+    {
+        $this->setHotels();
+
+        return view("index");
+    }
+
+    private function setHotels(): void
+    {
+        $this->hotels = (new ApiAction)->getHotels();
     }
 }
