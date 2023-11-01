168fa57368deab6a9a1395b813c5632a66932d80
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-11-01 11:55:26 +0000

    create hotels table

---
+++ database/migrations/2023_11_01_111704_create_hotels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->ulid('id');
+            $table->primary('id');
+
+            $table->point('latitude')->index();
+            $table->point('longitude')->index();
+            $table->decimal('price_per_night', $precision = 8, $scale = 2)->index();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
