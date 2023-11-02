dcb6944aafc4936798bfe0fb86ed0811c07dafa6
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-11-02 20:05:10 +0000

    search with proximity

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use App\Actions\ApiAction;
 use App\Models\Hotel;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class SearchController extends Controller
 {
     private Collection $hotels;
+    private float $latitude;
+    private float $longitude;
     private string $orderBy;
 
     public function index(): View
@@ -18,17 +21,31 @@ class SearchController extends Controller
         return view("index");
     }
 
-    public function search(): View
+    public function search(): RedirectResponse
     {
+        $this->latitude = request()->latitude;
+        $this->longitude = request()->longitude;
         $this->orderBy = request()->order_by;
 
         $this->setHotels();
 
-        return view("index")->with('hotels', $this->hotels);
+        session()->flash('hotels', $this->hotels);
+
+        return back()->withInput();
     }
 
     private function setHotels(): void
     {
-        $this->hotels = Hotel::orderBy($this->orderBy)->get();
+        $this->hotels = Hotel::select(
+            '*',
+            DB::raw("
+                ST_Distance_Sphere(
+                    point(longitude, latitude),
+                    point($this->longitude, $this->latitude)
+                ) / 1000 AS proximity"
+            )
+        )
+        ->orderByRaw("ISNULL($this->orderBy), $this->orderBy ASC")
+        ->get();
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,30 +9,38 @@
 
             <div>
                 <label for="latitude">Latitude</label>
-                <input id="latitude" maxlength="21" name="latitude" type="text" required>
+                <input id="latitude" maxlength="21" name="latitude" type="text"
+                    value="{{ old('latitude') }}" required>
             </div>
 
             <div style="margin-top: 5px">
                 <label for="longitude">Longitude</label>
-                <input id="longitude" maxlength="21" name="longitude" type="text" required>
+                <input id="longitude" maxlength="21" name="longitude"
+                    value="{{ old('longitude') }}" type="text" required>
             </div>
 
             <div style="margin-top: 5px">
                 <label for="order_by">Order by</label>
                 <select name="order_by">
-                    <option value="proximity">Proximity</option>
-                    <option value="price_per_night">Price per night</option>
+                    <option value="proximity"
+                        @selected(old('order_by') == 'proximity')
+                    >Proximity</option>
+                    <option value="price_per_night"
+                        @selected(old('order_by') == 'price_per_night')
+                    >Price per night</option>
                 <select>
             </div>
 
             <input style="margin-top: 5px" type="submit" value="Search">
         </form>
 
-        @isset ($hotels)
+        @if (session()->has('hotels'))
             <ul>
-                @foreach ($hotels as $hotel)
+                @foreach (session('hotels') as $hotel)
                     <li>
-                        {{ $hotel->name }}, 1km, {{ $hotel->price_per_night }} EUR
+                        {{ $hotel->name }},
+                        {{ $hotel->proximity != 'Without' ? round($hotel->proximity, 2) : 'tt' }} km,
+                        {{ $hotel->price_per_night }} EUR
                     </li>
                 @endforeach
             </ul>
